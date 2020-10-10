b06ac79ec6753d8fb6d195bf2ba0b6bcf6ba78ae
Minja Popović <prophethor@gmail.com>
2020-10-10 23:58:28 +0200

    Main mechanic implemented

---
+++ SpaceCord/Assets/Scripts/Background.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+    public GameObject background;
+    public Transform left, right;
+
+    private Material bgMat;
+
+    [HideInInspector]
+    public static float camLeft, camRight, camUp, camDown;
+
+    void CalculateCamBorders (float distance) {
+
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, Mathf.Clamp(Mathf.Abs(distance) / 1.5f, 5, 8),0.01f); 
+        //background.transform.localScale = new Vector3(Camera.main.orthographicSize * 4, Camera.main.orthographicSize * 4);
+
+        Vector3 newPos = Vector3.Lerp(Camera.main.transform.position, (left.transform.position + right.transform.position) / 2, 0.007f);
+        newPos.z = -10;
+        Camera.main.transform.position = newPos;
+
+        camLeft = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.rect.x, 0)).x;
+        camRight = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0f)).x;
+        camUp = Camera.main.ScreenToWorldPoint(new Vector3(0f, Camera.main.pixelHeight)).y;
+        camDown = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.rect.y, 0)).y;
+    }
+
+    void Start()
+    {
+        bgMat = background.GetComponent<MeshRenderer>().material;
+    }
+
+    void Update()
+    {
+        Vector2 dir = left.transform.position - right.transform.position;
+        CalculateCamBorders(dir.magnitude);
+        bgMat.mainTextureOffset += Vector2.one * 0.00025f;
+    }
+}
---
+++ SpaceCord/Assets/Scripts/CharacterControls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CharacterControls : MonoBehaviour
+{
+    public GameObject target;
+    public Rigidbody2D missile;
+    
+    public string axisX, axisY, shootButton;
+    public float speed = 10f;
+    public float missileSpeed = 15;
+
+    private float horMov, verMov;
+    private Vector2 dir;
+
+    void Update()
+    {
+        dir = target.transform.position - transform.position;
+
+        horMov = Input.GetAxis(axisX);
+        verMov = Input.GetAxis(axisY);
+        if (horMov > 0 && transform.position.x > Background.camRight) horMov = 0;
+        else if (horMov < 0 && transform.position.x < Background.camLeft) horMov = 0;
+        if (verMov > 0 && transform.position.y > Background.camUp) verMov = 0;
+        else if (verMov < 0 && transform.position.y < Background.camDown) verMov = 0;
+
+        transform.position += new Vector3(horMov, verMov) * 1/speed;
+        transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(dir.x,dir.y) * Mathf.Rad2Deg);
+
+        if (Input.GetButtonDown(shootButton)) Shoot(missile);
+    }
+
+    private void Shoot(Rigidbody2D missile) {
+        Rigidbody2D rb = Instantiate(missile,transform.position+transform.up*0.5f,Quaternion.identity);
+        rb.rotation = -Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+        rb.velocity = dir.normalized*missileSpeed;
+    }
+}
